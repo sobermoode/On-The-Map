eb1f57f9bfdf4c440e44c4902545c110a50a7d38
Aaron <J>
2015-06-12 08:47:47 -0700

    Added the keyboard-hiding tap gesture recognizer to the new info posting view.

--- On The Map/InformationPostingView2Controller.swift
+++ On The Map/InformationPostingView2Controller.swift
@@ -14,11 +14,28 @@ class InformationPostingView2Controller: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        // NOTE:
+        // code taken from http://stackoverflow.com/a/27079103,
+        // as per the suggestion from the code review
+        // Looks for single or multiple taps.
+        var tap: UITapGestureRecognizer = UITapGestureRecognizer(
+            target: self,
+            action: "DismissKeyboard"
+        )
+        view.addGestureRecognizer( tap )
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // Calls this function when the tap is recognized.
+    func DismissKeyboard()
+    {
+        // Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing( true )
     }
     
 
